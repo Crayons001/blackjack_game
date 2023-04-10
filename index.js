996489f2555cdfac8acddfd5cabda03c0489b763
Crayons001 <immanuelochi@gmail.com>
2023-04-10 15:13:50 +0300

    Added functionality to 'start game' button

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 let firstCard = 6
 let secondCard = 15
 let isWinner = false
+let isAlive = true
 
 let sum = firstCard + secondCard
+let message = ""
 
-if (sum <= 20)
+function startGame()
 {
-    console.log("Do you want to print out a new card?")
-}
-else if (sum === 21)
-{
-    console.log("You've got blackjack!")
-    isWinner = true
-}
-else
-{
-    console.log("You're out of the game")
+    if (sum <= 20)
+    {
+        message = "Do you want to print out a new card?"
+        console.log(message)
+    }
+    else if (sum === 21)
+    {
+        message = "You've got blackjack!"
+        isWinner = true
+    }
+    else
+    {
+        message = "You're out of the game"
+        isAlive = false
+    }
+
+    console.log(message)
 }
